929e157db782f619ee1095e4b41fc267edfb42ec
Rahul <rahulchoudhary@Rahuls-Laptop.local>
2024-10-18 16:36:16 +0530

    basic rewact

---
+++ crud-app/src/App.jsx
@@ -0,0 +1,32 @@
+
+const App=()=>{
+  return (
+    <div style={{
+      background:"#ddd",
+      minHeight:"100vh"
+    }}>
+    <div style={{
+      width:"70%",
+      background:"white",
+      margin:"32px auto"
+    }}>
+    <h1 style={{
+      padding:0,
+      margin:0,
+      textAlign:"center"
+    }}>
+    CodingOtt
+    </h1>
+    <button style={{
+      border:"none",
+      backgroundColor:"#8407ba",
+      color:"white",
+      padding:"14px 32px",
+      
+    }}>New Student</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
